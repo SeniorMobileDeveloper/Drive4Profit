cca08c16c3fa4cb4ab902d86fe8a3c9981f34b5f
XinMa <xinma@XinMas-Mac-Pro.local>
2014-08-14 16:29:06 +0800

    Initial Commit

---
+++ IDEE/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IDEE
+//
+//  Created by XinMa on 8/14/14.
+//  Copyright (c) 2014 Jin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IDEE/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IDEE
+//
+//  Created by XinMa on 8/14/14.
+//  Copyright (c) 2014 Jin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
